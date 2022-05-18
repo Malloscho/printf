199251aa9aad0c6701a2aad228eec80e08c5b8b8
Mario <mario.scholz@stud.thga.de>
2022-05-18 21:50:35 +0200

    addet farbige ASCII

---
+++ ansi_color.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+	    
+
+	    printf("\033[42;1m      O        \033[0m\n");	        
+	    printf("\033[42;1m      |        \033[0m\n");
+	    printf("\033[42;1m   --( )--     \033[0m\n");
+            printf("\033[42;1m     ( )       \033[0m\n");
+	    printf("\033[42;1m     | |       \033[0m\n");
+            printf("\033[42;1m     | |       \033[0m\n");
+
+	    return 0;
+}
